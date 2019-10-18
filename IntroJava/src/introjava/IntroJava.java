94687713255c087a0ed404438547d0f58b9cda2e
Richar <rsguayax@gmail.com>
2019-10-17 20:25:38 -0500

    cambios

---
+++ IntroJava/src/introjava/IntroJava.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introjava;
+
+/**
+ *
+ * @author Salas
+ */
+public class IntroJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bienvenidos a la programación en Java");
+        System.out.println("Este código va ha ser subido en github en el repositorio: programacion1920");
+    
+    }
+    
+}
